9a8fa2678c21631e576e0bc9be0d0d2fff0dba30
Satveer Singh Mechu <mechu.satveer@gmail.com>
2014-12-23 00:33:24 +0530

    Create CodeReview.cpp

---
+++ CodeReview.cpp
@@ -0,0 +1,11 @@
+Code review points
+
+    General Unit Testing
+    Comment and Coding Conventions
+    Error Handling
+    Resource Leaks
+    Thread Safety
+    Control Structures
+    Performance
+    Functionality
+    Security
