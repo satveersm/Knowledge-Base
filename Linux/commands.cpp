73285c0baf325cf79c413558aeab23d23162ddca
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-17 22:34:19 +0530

    Create commands.cpp

---
+++ Linux/commands.cpp
@@ -0,0 +1,7 @@
+What command is used to count the total number of lines, words, and characters contained in a file?
+
+wc  -c file_name print bytes in a file
+wc  -m file_name char count in file
+wc -l  file_name new line count in file
+wc -L file_name longest line length
+wc -w file_name word count
