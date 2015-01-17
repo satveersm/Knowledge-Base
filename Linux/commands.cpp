73247aa6468116483acc10d0b7f82f23ad827efc
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-17 23:01:03 +0530

    Update commands.cpp

--- Linux/commands.cpp
+++ Linux/commands.cpp
@@ -1,7 +1,34 @@
-What command is used to count the total number of lines, words, and characters contained in a file?
+1)What command is used to count the total number of lines, words, and characters contained in a file?
 
 wc  -c file_name print bytes in a file
 wc  -m file_name char count in file
 wc -l  file_name new line count in file
 wc -L file_name longest line length
 wc -w file_name word count
+
+2)What TCP/IP protocol is used for remote terminal connection service?
+telnet
+3)What service is used to translate domain names to IP addresses?
+dns
+4)What command is used to sort the lines of data in a file in alphabetical order?
+sort
+5)What command is used with vi editor to search a pattern in the forward direction?
+/
+6)What command is used to save the standard output in a file, as well as display it on the terminal?
+tee -a filename
+7)What command is used to sort the lines of data in a file in reverse order?
+sort -r
+8)What command is used with vi editor to move forward to the end of a word?
+e
+9)What can he used to set up a firewall on a Linux system?
+ipchains
+10)Which of the following command lists the last 10 lines of a file?
+tail n file_name
+11)What command is used with vi editor to delete three words?
+3 dw
+12) how to delete a line
+dd
+13)What command is used to summarize the disk usage?
+du
+
+
