004c716b677c04ad47f561e0be4e50b01c92c483
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-18 12:26:41 +0530

    Update commands.cpp

--- Linux/commands.cpp
+++ Linux/commands.cpp
@@ -32,5 +32,36 @@ dd
 du
 14)How to access Linux systems remotely from Windows machine? 
 SSH Secure Shell Client or putty
+15)Vi commands
+create file  vi hello.txt
+to start writing data press insert or i
+to save  esc and wq!
+to quit without saving q!
+16) To display contents of file
+cat filename
+17) to copy contents of one file to other file
+cp file1 file2
+mkdir junk
+cp file1 ./junk/file2.txt
+
+18) to go one level up from current dir
+cd ..
+19) to remove a file
+rm file1
+20) to remove a dir
+rm -r dirname
+21) rename a file
+mv file  newname
+22) man command_name to get help abt any command
+23)Use the diff command to see the differences in two files 
+diff test.txt newtest3.txt
+24)Previously executed commands in a shell can be 
+viewed by using the history command
+history
+25)If the contents to display are more than one page, 
+you could use the more/less command for paging 
+through text a screenful at a time
+more test.txt 
+less test.txt
 
 
