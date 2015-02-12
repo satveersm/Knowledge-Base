6f930601fd004a7025829fc8f35d4a965884dd84
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-12 23:57:06 +0530

    Create Note3.cpp

---
+++ Database/Note3.cpp
@@ -0,0 +1,32 @@
+
+What’s the difference between data mining and data warehousing?
+ 
+Data mining is the process of finding patterns in a given data set. 
+These patterns can often provide meaningful and insightful data to 
+whoever is interested in that data. Data mining is used today in a 
+wide variety of contexts – in fraud detection, as an aid in marketing
+campaigns, and even supermarkets use it to study their consumers.
+
+Data warehousing can be said to be the process of centralizing or 
+aggregating data from multiple sources into one common repository  
+
+
+If you’ve ever used a credit card, then you may know that credit card
+companies will alert you when they think that your credit card is being 
+fraudulently used by someone other than you. This is a perfect example 
+of data mining – credit card companies have a history of your purchases 
+the past and know geographically where those purchases have been made. 
+If all of a sudden some purchases are made in a city far from where you live,
+the credit card companies are put on alert to a possible fraud since their
+data mining shows that you don’t normally make purchases in that city. 
+Then, the credit card company can disable your card for that transaction or 
+just put a flag on your card for suspicious activity.
+
+Another interesting example of data mining is how one grocery store in the 
+USA used the data it collected on it’s shoppers to find patterns in their shopping habits.
+They found that when men bought diapers on Thursdays and Saturdays, they also had a 
+strong tendency to buy beer. The grocery store could have used this valuable information 
+to increase their profits. One thing they could have done – odd as it sounds – is move the
+beer display closer to the diapers. Or, they could have simply made sure not to give any
+discounts on beer on Thursdays and Saturdays. This is data mining in action – extracting meaningful
+data from a huge data set.
