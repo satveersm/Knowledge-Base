3f4396160961b87689da5b6209bc375b1d30d060
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-17 16:27:06 +0530

    Update OBIEE.cpp

--- OBIEE.cpp
+++ OBIEE.cpp
@@ -1,1 +1,4 @@
 http://rws65742fwks.us.oracle.com/envjango
+https://blogs.oracle.com/blogbypuneeth/resource/IDP_Credential_Mapper.jpg
+
+http://fusionsecurity.blogspot.co.uk/2012/06/integrating-obiee-11g-into-weblogics.html
