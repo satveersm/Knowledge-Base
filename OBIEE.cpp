015f19791862f9a827af5a2334148e7eb957a90e
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-17 21:20:38 +0530

    Update OBIEE.cpp

--- OBIEE.cpp
+++ OBIEE.cpp
@@ -2,3 +2,8 @@ http://rws65742fwks.us.oracle.com/envjango
 https://blogs.oracle.com/blogbypuneeth/resource/IDP_Credential_Mapper.jpg
 
 http://fusionsecurity.blogspot.co.uk/2012/06/integrating-obiee-11g-into-weblogics.html
+
+
+SAML2.0
+
+http://biemond.blogspot.in/2009/05/sso-with-weblogic-103-and-saml.html
